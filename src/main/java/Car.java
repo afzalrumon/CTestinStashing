7716f714afd29b55c649fb1a6b3f5d7228a01db3
afzalrumon <a.hussain.rumon@outlook.com>
2021-06-12 11:30:26 -0400

    Car object created with details

---
+++ src/main/java/Car.java
@@ -0,0 +1,7 @@
+public class Car {
+    String color;
+    String number;
+    String model;
+    String door;
+
+}
---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args) {
+        Car audi=new Car();
+       audi.color="black";
+       audi.door="four";
+       audi.model="r8";
+       audi.number="00790";
+       
+
+
+    }
+}
